b5e44c3886928174e002f709fb1a28f90760c099
TimotiusEk <eka.valentino@gmail.com>
2017-10-20 11:09:18 +0700

    First commit (including 360 video and main menu)

---
+++ app/src/main/java/com/example/timotiusek/runcompanionvr/HomeActivity.java
@@ -0,0 +1,38 @@
+package com.example.timotiusek.runcompanionvr;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if(getSupportActionBar() != null)
+            getSupportActionBar().hide();
+        setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.warp_me_cardview)
+    public void warpMe(){
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void comingSoon(View v){
+        Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show();
+    }
+}
